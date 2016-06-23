6c31e7661273e9eca59c440eb016cbf7f445bcdf
julianbei <julian.beisenkoetter@hitfoxgroup.com>
2016-06-23 15:04:37 +0200

    refactor for reusability

--- src/directives/form.directive.js
+++ src/directives/form.directive.js
@@ -1,99 +1,101 @@
-"use strict";
+'use strict';
 
 /**
  * @ngdoc directive
  * @name form
- * @requires $parse 
+ * @requires $parse
  * @requires $rootScope
- * 
+ *
  * @description
- * Using form element as directive, we don't require to put the "bs-validation" directive to every form element.
+ * Using form element as directive, we don't require to put the 'bs-validation' directive to every form element.
  * To add handler on submit, use <code>on-submit</code> instead of <code>ng-submit</code>,
  * since ng-submit directive doesn't cares about validation errors.
  */
-angular.module("bootstrap.angular.validation").directive("form", ["$parse", "$rootScope", "BsValidationService",
-        function($parse, $rootScope, bsValidationService) {
-
+angular.module('bootstrap.angular.validation').directive('form', [
+  '$parse',
+  '$rootScope',
+  'BsValidationService',
+  function($parse, $rootScope, bsValidationService) {
     return {
-        restrict: "E",
-        require: "form",
-        priority: 1000,     // Setting a higher priority so that, this directive compiles first.
-        compile: function($formElement, $formAttributes) {
-            // Disable HTML5 validation display
-            $formElement.attr("novalidate", "novalidate");
-            bsValidationService.addDirective($formElement);
+      restrict: 'E',
+      require: 'form',
+      priority: 1000,     // Setting a higher priority so that, this directive compiles first.
+      compile: function($formElement, $formAttributes) {
+          // Disable HTML5 validation display
+          $formElement.attr('novalidate', 'novalidate');
+          bsValidationService.addDirective($formElement);
+
+          /**
+           * If there is an 'ng-include' directive available inside a form then the 'bs-validation' directive
+           * won't get applied until Angular resolves the view sourced by 'ng-include'.
+           */
+          var nestedNgIncludeElement = $formElement.findAll('[ng-include]');
+          if (nestedNgIncludeElement.length > 0) {
+            var src = $parse(nestedNgIncludeElement.attr('ng-include'))();
 
             /**
-             * If there is an "ng-include" directive available inside a form then the "bs-validation" directive
-             * won't get applied until Angular resolves the view sourced by "ng-include".
+             * Then add the source URL of ng-include to a list so that in the response interceptor, we can add
+             * the 'bs-validation' directive. We can do this by recompiling here the content after the content is
+             * loaded but that leads to some problem and also increases the rendering time.
+             *
+             * @see response interceptor in app.js
              */
-            var nestedNgIncludeElement = $formElement.findAll("[ng-include]");
-            if (nestedNgIncludeElement.length > 0) {
-                var src = $parse(nestedNgIncludeElement.attr("ng-include"))();
+            bsValidationService.addToNgIncludedURLs(src);
+          }
 
-                /**
-                 * Then add the source URL of ng-include to a list so that in the response interceptor, we can add
-                 * the "bs-validation" directive. We can do this by recompiling here the content after the content is
-                 * loaded but that leads to some problem and also increases the rendering time.
-                 * 
-                 * @see response interceptor in app.js
+          var ngSubmit = $formAttributes.ngSubmit;
+          /*
+           * Removing ngSubmit attribute if any since ngSubmit by default doesn't respects the validation errors
+           * on the input fields.
+           */
+          delete $formAttributes.ngSubmit;
+
+          var preLinkFunction = function($scope, formElement, $attr, formController) {
+            // Expose a method to manually trigger the validation
+            formController.$validate = function() {
+                formController.$setSubmitted();
+            };
+
+            formElement.on('submit', function(e) {
+              e.preventDefault();
+
+              // If any of the form element has not passed the validation
+              if (formController.$invalid) {
+                // Then focus the first invalid element
+                formElement[0].querySelector('.ng-invalid').focus();
+                return false;
+              }
+
+              // Parse the handler of ng-submit & execute it
+              var submitHandler = $parse(ngSubmit);
+              $scope.$apply(function() {
+                submitHandler($scope, {$event: e});
+
+                formController.$commitViewValue();
+                formController.$setSubmitted();
+
+                /*
+                 * Do not show validation errors once the form gets submitted. You can still display the
+                 * validation errors after form submission by calling '$setSubmitted' in your form controller.
                  */
-                bsValidationService.addToNgIncludedURLs(src);
-            }
+                formController.$setPristine();
+              });
 
-            var ngSubmit = $formAttributes.ngSubmit;
-            /*
-             * Removing ngSubmit attribute if any since ngSubmit by default doesn't respects the validation errors
-             * on the input fields.
-             */
-            delete $formAttributes.ngSubmit;
+              /**
+               * Prevent other submit event listener registered via Angular so that we can mark the form with
+               * the prestine state. Otherwise, that Angular's listener is getting called at the last and is again
+               * setting form to the submitted.
+               *
+               * https://api.jquery.com/event.stopimmediatepropagation/
+               */
+              e.stopImmediatePropagation();
+              return false;
+            });
+          };
 
-            var preLinkFunction = function($scope, formElement, $attr, formController) {
-                // Expose a method to manually trigger the validation
-                formController.$validate = function() {
-                    formController.$setSubmitted();
-                };
-
-                formElement.on("submit", function(e) {
-                    e.preventDefault();
-
-                    // If any of the form element has not passed the validation
-                    if (formController.$invalid) {
-                        // Then focus the first invalid element
-                        formElement[0].querySelector(".ng-invalid").focus();
-                        return false;
-                    }
-
-                    // Parse the handler of ng-submit & execute it
-                    var submitHandler = $parse(ngSubmit);
-                    $scope.$apply(function() {
-                        submitHandler($scope, {$event: e});
-
-                        formController.$commitViewValue();
-                        formController.$setSubmitted();
-
-                        /*
-                         * Do not show validation errors once the form gets submitted. You can still display the
-                         * validation errors after form submission by calling "$setSubmitted" in your form controller.
-                         */
-                        formController.$setPristine();
-                    });
-
-                    /**
-                     * Prevent other submit event listener registered via Angular so that we can mark the form with
-                     * the prestine state. Otherwise, that Angular's listener is getting called at the last and is again
-                     * setting form to the submitted.
-                     *
-                     * https://api.jquery.com/event.stopimmediatepropagation/
-                     */
-                    e.stopImmediatePropagation();
-                    return false;
-                });
-            };
-
-            return {
-                pre: preLinkFunction
-            };
-        }
+          return {
+            pre: preLinkFunction
+          };
+      }
     };
 }]);
--- src/directives/validation.directive.js
+++ src/directives/validation.directive.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 /**
  * @ngdoc directive
@@ -7,143 +7,128 @@
  * @requires BsValidationService
  * @description
  * This directive must be applied to every input element on which we need to use custom validations like jQuery.
- * Those element must have ng-model attributes. This directive will automatically add the "has-error" class on the
- * parent element with class ".form-group" and will show/hide the validation message automatically.
- * 
- * Custom supported validations are:
- * 
- * 1. For number, add attribute number instead of type number in input class (for int, float & double),
- * 2. For digits, add attribute digits (only for int),
- * 3. For minimum length validation, use HTML5 minlength attribute (Will work for non HTML5 browsers),
- * 4. For maximum length validation, use HTML5 maxlength attribute (Will work for non HTML5 browsers),
- * 5. For max number validation, use HTML5 max attribute,
- * 6. For min number validation, use HTML5 min attribute,
- * 7. For required validation, use HTML5 required attribute.
- * 
- * On any validation error, a default message will be shown to users, which can be customized dynamically with the
- * element itself. Hence this will work great for Angular UI validation.
- */
-angular.module("bootstrap.angular.validation").directive("bsValidation", ["$interpolate", "BsValidationService", function($interpolate, bsValidationService) {
-    return {
-        restrict: "A",
-        require: ["ngModel", "^^form"],
-        link: function($scope, $element, $attr, controllers) {
+ * Those element must have ng-model attributes. This directive will automatically add the 'has-error' class on the
+ * parent element with class '.form-group' and will show/hide the validation message automatically.
+*/
+
+angular.module('bootstrap.angular.validation').directive('bsValidation', [
+  '$interpolate', 'BsValidationService',
+    function($interpolate, bsValidationService, $log) {
+      return {
+          restrict: 'A',
+          require: ['ngModel', '^^form'],
+          link: function($scope, $element, $attr, controllers) {
+            // initialize controllers
             var ngModelController = controllers[0];
             var ngFormController = controllers[1];
 
-            var errorElementClass = "bs-invalid-msg";
+            // HTML selector helper
+            var errorElementClass = 'bs-invalid-msg';
+            var iconMarkup = '<i class=\'fa fa-exclamation-triangle fa-fw\'></i>';
+            var formGroup = '.form-group';
+            var customFormGroup = '[bs-form-group]';
+            var helpBlock = 'help-block';
+
             // All classed needed to add to validation message
-            var errorClasses = [errorElementClass, "help-block"];
-            var markupClasses = errorClasses.join(" ");
+            var errorClasses = [errorElementClass, helpBlock];
+            var markupClasses = errorClasses.join(' ');
 
             // Search parent element with class form-group to operate on.
-            var formGroupElement = $element.parents(".form-group");
+            var formGroupElement = $element.parents(formGroup);
 
-            // Search for an attribute "bs-form-group" if the class ".form-group" is not available
+            // Search for an attribute 'bs-form-group' if the class '.form-group' is not available
             if (!formGroupElement || formGroupElement.length === 0) {
-                formGroupElement = $element.parents("[bs-form-group]");
+              formGroupElement = $element.parents(customFormGroup);
 
-                // If we still don't find any element
-                if (!formGroupElement || formGroupElement.length === 0) {
-                    // Then do not execute the directive at all
-                    return;
-                }
+              // If we still don't find any element
+              if (!formGroupElement || formGroupElement.length === 0) {
+                // Then do not execute the directive at all
+                return;
+              }
             }
 
-            /*
-             * Used to resolve the message for current validation failure. Will first search the title attribute
-             * with the validation key and then fallback to use the default message.
-             */
-            function resolveMessage(element, key) {
-                /*
-                 * First search if the input element has a custom message for a particular validation key. For example:
-                 * Check if "title-required" attribute is present on the element to display the validation error message
-                 * when the field is required.
-                 */
-                var message = element.attr("title-" + key);
-
-                if (!message) {
-                    message = bsValidationService.getDefaultMessage(key);
-
-                    if (!message) {
-                        console.warn("Not found any message for the key", key);
-                        return "Please fix the error.";
-                    }
-                }
-
-                // Format the string if it contains any variable. Like <code>String.format()</code> in Java.
-                return $interpolate(message)({validValue: $attr[key]});
-            }
-
-            /**
-             * Display or hide validation errors for a single element.
-             */
-            function displayOrHideError() {
-                /*
-                 * Do not show or hide error for current element don't have any validation errors or has validation
-                 * error but user has not attempted to submit the form yet.
-                 */
-                if (!ngFormController.$submitted || !ngModelController.$invalid) {
-                    formGroupElement.removeClass("has-error");
-
-                    if (formGroupElement.length > 0) {
-                        formGroupElement.findAll("span." + errorClasses.join(".")).addClass("ng-hide");
-                    }
-                    return false;
-                }
-
-                /**
-                 * Display the first error for the current element. For multiple validations, this $error object will
-                 * be like this:
-                 *
-                 * $error = {
-                 *     required: true,  // When field is marked as required but not entered.
-                 *     minlength: true,
-                 *     number: true
-                 * }
-                 */
-                var allErrorKeys = Object.keys(ngModelController.$error);
-                var firstErrorKey = allErrorKeys[0];
-
-                // Add bootstrap error class
-                formGroupElement.addClass("has-error");
-
-                var message = resolveMessage($element, firstErrorKey);
-
-                // Find if the parent class already have error message container
-                var errorElement = formGroupElement.findOne("." + errorElementClass);
-                var iconMarkup = "<i class=\"fa fa-exclamation-triangle fa-fw\"></i>";
-
-                // If not, then append an error container
-                if (errorElement.length === 0) {
-                    var insertAfter = $element;
-                    // Check if the container have any Bootstrap input group then append the error after it
-                    var groupElement = formGroupElement.findOne(".input-group");
-                    if (groupElement.length > 0) {
-                        insertAfter = groupElement;
-                    }
-
-                    var errorMarkup = "<span class=\"" + markupClasses + "\">" + iconMarkup + message + "</span>";
-                    insertAfter.after(errorMarkup);
-                } else {
-                    // Else change the message.
-                    errorElement.html(iconMarkup + message).removeClass("ng-hide");
-                }
-            }
-
-            var validators = ["equalTo", "min", "max", "number", "digits", "length"];
-            // Register generic custom validators if added to element
-            angular.forEach(validators, function(key) {
-                var attrValue = $element.attr(key);
-                if (attrValue || (typeof attrValue !== "undefined" && attrValue !== false) || $attr[key]) {
-                    bsValidationService.addValidator($scope, $attr, ngModelController, key);
-                }
+            var metaInformation = {};
+            angular.forEach(bsValidationService.getMeta(), function(key){
+              metaInformation[key] = $element.attr(key);
             });
 
-            // Look when user try to submit the form & display validation errors.
+            // Register generic custom validators if added to element
+            angular.forEach(bsValidationService.getValidators(), function(key) {
+              var attrValue = $element.attr(key);
+              if ($attr[key] || (angular.isDefined(attrValue) && attrValue !== false)) {
+                bsValidationService.addValidator($scope, $attr, ngModelController, key, metaInformation);
+              }
+            });
+
+            // register watchers for submission touch and valid
             $scope.$watchCollection(function() {
-                return ngFormController.$submitted && ngModelController.$error;
+              return ngFormController.$submitted && ngModelController.$error;
             }, displayOrHideError);
-        }
-    };
-}]);
+
+            $scope.$watchCollection(function() {
+              return ngModelController.$touched;
+            }, displayOrHideError);
+
+            $scope.$watchCollection(function() {
+              return ngModelController.$valid
+            }, displayOrHideError);
+
+            function resolveMessage(key) {
+              var message = $element.attr(key + '-notification') || bsValidationService.getDefaultMessage(key);
+              var matchers = angular.extend({}, {validValue: $attr[key]}, metaInformation);
+              return $interpolate(message)(matchers);
+            }
+
+            function removeErrors() {
+              formGroupElement.removeClass('has-error');
+              if (formGroupElement.length > 0) { formGroupElement.findAll('span.' + errorClasses.join('.')).addClass('ng-hide'); }
+              return false;
+            }
+
+            function addErrors(){
+              addErrorClass();
+              addErrorMessage();
+              return false;
+            }
+
+            function addSuccessClass(){
+              formGroupElement.addClass('has-success');
+              return removeErrors();
+            }
+
+            function addErrorClass(){
+              formGroupElement.removeClass('has-success');
+              formGroupElement.addClass('has-error');
+            }
+
+            function renderError(message){
+              return '<span class="' + markupClasses + '">' + iconMarkup + message + '</span>';
+            }
+
+            function errorContainer(message){
+              var insertAfter = $element;
+              // Check if the container have any Bootstrap input group then append the error after it
+              var groupElement = formGroupElement.findOne('.input-group');
+              if (groupElement.length > 0) {
+                  insertAfter = groupElement;
+              }
+              insertAfter.after(renderError(message));
+            }
+
+            function addErrorMessage(){
+              var message = resolveMessage(Object.keys(ngModelController.$error)[0]);
+              var errorElement = formGroupElement.findOne('.' + errorElementClass);
+              if (errorElement.length === 0) { errorContainer(message) }
+              errorElement.html(iconMarkup + message).removeClass('ng-hide');
+            }
+
+            function displayOrHideError() {
+              if(ngModelController.$valid){ return addSuccessClass(); }
+              if(ngModelController.$untouched){ return removeErrors(); }
+              if(ngModelController.$invalid){ return addErrors(); }
+            }
+
+          }
+        };
+    }
+]);
