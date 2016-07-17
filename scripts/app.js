ead06a12e00241805816afde93339500c98506be
Shashank Agrawal <shagrawal99@gmail.com>
2016-07-17 11:50:30 +0530

    Using a directive to render the codepen example.

--- scripts/app.js
+++ scripts/app.js
@@ -18,3 +18,26 @@ angular.module('bsValidationApp')
         .otherwise('/');
     }
   ]);
+
+angular.module('bsValidationApp').directive('codepen', ['$interpolate', '$sce', function($interpolate, $sce) {
+  return {
+    restrict: 'E',
+    replace: true,
+    template: function() {
+      return '<iframe height="{{::height}}" scrolling="no" src="{{::src}}" frameborder="no"' +
+        ' allowtransparency="true" allowfullscreen="true" style="width: 100%;">See the Pen ' +
+        '<a href="https://codepen.io/shashank-agrawal/pen/akEXbr/">Bootstrap + Angular Validation </a> ' +
+        'by Shashank Agrawal (<a href="http://codepen.io/shashank-agrawal">@shashank-agrawal</a>) ' +
+        'on <a href="http://codepen.io">CodePen</a>.</iframe>';
+    },
+    link: function($scope, $element, $attr) {
+      $scope.height = $attr.height || 300;
+      $scope.themeId = $attr.themeId || 'light';
+      $scope.penId = $attr.penId;
+
+      var src = '//codepen.io/shashank-agrawal/embed/{{::penId}}/?height={{::height}}&theme-id={{::themeId}}&default-tab=result&embed-version=2';
+      src = $interpolate(src)($scope);
+      $scope.src = $sce.trustAsResourceUrl(src);
+    }
+  };
+}]);
